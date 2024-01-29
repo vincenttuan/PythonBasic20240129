f95b28ce4060c1dcb1504501a9aa9daeeb85c39c
vincentjava <vincentjava@yahoo.com.tw>
2024-01-29 11:55:18 +0800

    python

---
+++ day1/hello8.py
@@ -0,0 +1,14 @@
+# 人機互動 input
+# 使用者可以自行輸入身高與體重
+# 並且可以判斷是否正常,過輕或過重
+# 18 < bmi <= 23 正常, bmi <= 18 過輕, bmi > 23 過重
+import math
+
+h = float(input('請輸入身高(cm):'))
+w = float(input('請輸入體重(kg):'))
+print(h, w)
+print(type(h), type(w))
+bmi = w / math.pow(h/100, 2)  # w / (h/100)**2
+print(bmi)
+
+
