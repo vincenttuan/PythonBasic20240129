fe10325f7f7358e091b3c80f59a3a3eb6c0de401
vincentjava <vincentjava@yahoo.com.tw>
2024-01-30 11:11:08 +0800

    python

--- day2/test5.py
+++ day2/test5.py
@@ -1,6 +1,13 @@
 data1 = "170, 160.5, 180"  # 身高
 data2 = "60, 45, 89.5"     # 體重
 # 請將正常範圍的 18 < bmi <= 23 印出
+h_array = data1.split(", ")
+w_array = data2.split(", ")
+print(h_array, w_array)
+h1, h2, h3 = float(h_array[0]), float(h_array[1]), float(h_array[2])
+print(h1, h2, h3)
+w1, w2, w3 = float(w_array[0]), float(w_array[1]), float(w_array[2])
+print(w1, w2, w3)
 
 
 
