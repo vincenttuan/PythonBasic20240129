070cdcc2397ca52c6d2011a2da9566f2494adddc
vincentjava <vincentjava@yahoo.com.tw>
2024-01-30 11:42:38 +0800

    python

--- day2/test6.py
+++ day2/test6.py
@@ -2,9 +2,27 @@ def calcBmi(h, w):  # 自訂義一個計算並列印 bmi 函式
     bmi = w / (h/100)**2
     print("bmi = %.2f" % bmi)
 
+def getBmi(h, w):
+    bmi = w / (h/100)**2
+    return bmi  # 將 bmi 回傳
+
+def getBmiAndResult(h, w):
+    bmi = getBmi(h, w)
+    if bmi <= 18:
+        result = "過輕"
+    elif bmi > 23:
+        result = "過重"
+    else:
+        result = "正常"
+    return bmi, result
 
 if __name__ == '__main__':  # python 的主程式寫法
     calcBmi(170, 60)
     calcBmi(160, 50)
 
+    bmi = getBmi(180, 85)
+    print(bmi)
+
+    bmi, result = getBmiAndResult(175, 43)
+    print(bmi, result)
 
