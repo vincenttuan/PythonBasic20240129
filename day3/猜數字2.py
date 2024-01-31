c798a541a09dcd28b26a0b5f793fc350a30b1bc1
vincentjava <vincentjava@yahoo.com.tw>
2024-01-31 11:57:03 +0800

    python

---
+++ day3/猜數字2.py
@@ -0,0 +1,23 @@
+# 猜數字 1~99 區間提示版
+import random
+
+if __name__ == '__main__':
+    answer = random.randint(1, 99)
+    min = 1
+    max = 99
+    while True:
+        # 玩家猜
+        user_guess = int(input('請在 %d ~ %d 猜一數字:' % (min, max)))
+        # 判斷數字範圍
+        if user_guess < min or user_guess > max:
+            print('數字範圍錯誤, 請重猜~')
+            continue
+        # 判斷是否猜對 ?
+        if user_guess > answer:
+            max = user_guess - 1
+        elif user_guess < answer:
+            min = user_guess + 1
+        else:
+            print("玩家答對了")
+            break
+
