df322af8d10138b7db9a4cf47e1210d6d610fe1d
vincentjava <vincentjava@yahoo.com.tw>
2024-01-31 10:00:19 +0800

    python

---
+++ day3/demo1.py
@@ -0,0 +1,7 @@
+# 隨機數
+import random
+
+if __name__ == '__main__':
+    n = random.randint(1, 99)
+    print(n)
+   
---
+++ day3/demo2.py
@@ -0,0 +1,13 @@
+# 電腦選號 4 星彩
+# 0~9 隨機選出 4 個數字
+import random
+
+if __name__ == '__main__':
+    n = random.randint(0, 9)
+    print(n, end=" ")
+    n = random.randint(0, 9)
+    print(n, end=" ")
+    n = random.randint(0, 9)
+    print(n, end=" ")
+    n = random.randint(0, 9)
+    print(n, end=" ")
