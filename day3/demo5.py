6d83df87a7e1128e95a71b76668aeaa6d7bf50d9
vincentjava <vincentjava@yahoo.com.tw>
2024-01-31 10:18:46 +0800

    python

---
+++ day3/demo5.py
@@ -0,0 +1,10 @@
+# 電腦選號 3 星彩
+# 0~9 隨機選出 3 個數字
+import random
+
+from day3.demo4 import getNumberAndPrint
+
+if __name__ == '__main__':
+    getNumberAndPrint()
+    getNumberAndPrint()
+    getNumberAndPrint()
