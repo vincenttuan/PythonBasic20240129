efb016588fe5a704a64c3d86f516eba08883391c
vincentjava <vincentjava@yahoo.com.tw>
2024-01-31 11:32:27 +0800

    python

--- day3/猜數字.py
+++ day3/猜數字.py
@@ -7,6 +7,8 @@ import random
 if __name__ == '__main__':
     answer = random.randint(1, 9)
     count = 0  # 猜的次數
+    min = 1  # 預設最小值
+    max = 9  # 預設最大值
     while True:
         # 請玩家猜
         user_guess = int(input('玩家請在 1~9 猜一個數字:'))
@@ -17,13 +19,15 @@ if __name__ == '__main__':
         count = count + 1
         if user_guess > answer:
             print("猜大了")
+            max = user_guess - 1
         elif user_guess < answer:
             print("猜小了")
+            min = user_guess + 1
         else:
             print("恭喜玩家答對了, 總共猜了 %d 次" % count)
             break
         # 請電腦猜
-        pc_guess = random.randint(1, 9)
+        pc_guess = random.randint(min, max)
         print('電腦 1~9 所猜的數字是 %d' % pc_guess)
         if pc_guess > answer:
             print("電腦猜大了")
