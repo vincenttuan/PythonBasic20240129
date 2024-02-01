a35c11eca14097a460b938725f736f0d4be04752
vincentjava <vincentjava@yahoo.com.tw>
2024-02-01 09:19:22 +0800

    python

---
+++ day4/loop1.py
@@ -0,0 +1,8 @@
+# for 迴圈 (步進迴圈, 一步一步往下做)
+# 要印出 1~5
+for i in range(1, 6):  # 印出 1~小於6的資料
+    print(i, end=" ")
+# ---------------------------------------
+print()  # 換行
+for i in range(1, 6):
+    print(i, "Python", end=" ")  # 印出5個 Python 資料
