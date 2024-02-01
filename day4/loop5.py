2c55be30ed2555e9d28fd6d77ace037f728247e3
vincentjava <vincentjava@yahoo.com.tw>
2024-02-01 12:31:55 +0800

    python

--- day4/loop5.py
+++ day4/loop5.py
@@ -30,4 +30,31 @@ def get_preference(options, message):
     if choice in options:
         return options[choice]
 
+# 下單
+def order():
+    total_price = 0
+    order_items = []
+    # -------------------------------------------------------------------------
+    while True:
+        show_menu()
+        choice = int(input('請輸入飲料編號:'))
+        if choice in menu:
+            drink_name = menu[choice]["name"]
+            drink_price = menu[choice]["price"]
+            sugar = get_preference(sugar_options, "請選擇甜度")
+            ice = get_preference(ice_options, "請選擇冰塊量")
+            total_price += drink_price
+            print(f'您點了{drink_name} {sugar} {ice} ${drink_price}')
+            # 加入到 order_items 中
+            order_items.append((drink_name, sugar, ice, drink_price))
+            choice = input('是否要繼續點餐(y/n)?').lower()  # lower() 強制轉小寫
+            if choice != 'y':
+                break
+    #-------------------------------------------------------------------------
+    print('點餐完畢 ! 您的點餐明細:')
+    for drink_name, sugar, ice, drink_price in order_items:
+        print(f'明細: {drink_name} {sugar} {ice} ${drink_price}')
+    print(f'總金額: ${total_price}, 請至櫃台結帳, 謝謝光臨 !')
 
+if __name__ == '__main__':
+    order()
