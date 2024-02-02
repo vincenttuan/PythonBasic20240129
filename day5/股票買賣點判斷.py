ef19f789555ba00c34295bf35e08836ce1ffceb7
vincentjava <vincentjava@yahoo.com.tw>
2024-02-02 11:09:49 +0800

    python

---
+++ day5/股票買賣點判斷.py
@@ -0,0 +1,25 @@
+# 利用 twstock 來進行買賣點分析
+'''
+四大買賣點為:
+1.量大收紅/量大收黑
+2.量縮價不跌/量縮價跌
+3.三日均價由上往下/三日均價由下往上
+4.三日均價大於六日均價/三日均價小於六日均價
+'''
+import twstock
+
+if __name__ == '__main__':
+    print('四大買賣點判斷')
+    symbol = '2330'
+    bfp = twstock.BestFourPoint(twstock.Stock(symbol))
+    print('股票代號', symbol)
+    print('買進理由:')
+    print('量大收紅', bfp.best_buy_1())
+    print('量縮價不跌', bfp.best_buy_2())
+    print('三日均價由下往上', bfp.best_buy_3())
+    print('三日均價大於六日均價', bfp.best_buy_4())
+    print('賣出理由:')
+    print('量大收黑', bfp.best_sell_1())
+    print('量縮價跌', bfp.best_sell_2())
+    print('三日均價由上往下', bfp.best_sell_3())
+    print('三日均價小於六日均價', bfp.best_sell_4())
