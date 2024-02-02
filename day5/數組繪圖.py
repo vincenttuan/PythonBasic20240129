dff449708793cd51eb612c02170955df5fa42ebc
vincentjava <vincentjava@yahoo.com.tw>
2024-02-02 11:25:16 +0800

    python

--- day5/數組繪圖.py
+++ day5/數組繪圖.py
@@ -5,3 +5,8 @@ students = {'劉一': 90, '陳二': 85, '張三': 100, '李四': 45, '王五': 7
 
 if __name__ == '__main__':
      print(students)
+     # 分離學生名稱與分數
+     names = list(students.keys())
+     scores = list(students.values())
+     print(names)
+     print(scores)
