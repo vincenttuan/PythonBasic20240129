6e5dc42bd1fc9ac62ac12bbfa55c84c409780c39
vincentjava <vincentjava@yahoo.com.tw>
2024-02-02 11:29:11 +0800

    python

--- day5/數組繪圖.py
+++ day5/數組繪圖.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 # 學生成績
 # 繪圖需要安裝繪圖套件
 # pip install matplotlib
@@ -10,3 +11,12 @@ if __name__ == '__main__':
      scores = list(students.values())
      print(names)
      print(scores)
+
+     # 創建長條圖
+     plt.bar(names, scores, color='skyblue')
+     # 設定圖形標籤與標題
+     plt.xlabel('學生名稱')
+     plt.ylabel('分數')
+     plt.title('學生成績圖表')
+     # 顯示圖形
+     plt.show()
