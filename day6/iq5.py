9a9beba09dffe2e5265f22fe394cc4634b7d6d42
vincentjava <vincentjava@yahoo.com.tw>
2024-02-05 11:45:38 +0800

    python

---
+++ day6/iq5.py
@@ -0,0 +1,64 @@
+'''
+在上一題與下一題之間加入 radiobutton
+
++-----------------------------+
+            IQ 圖
+ 上一頁 A, B, C, D, E, F 下一頁
++-----------------------------+
+'''
+
+import tkinter as tk
+from PIL import ImageTk, Image
+
+def update_image(delta):
+    global current_image
+    current_image += delta
+    # 確保 current_image 在合法的範圍(1~20)
+    if current_image <= 0:
+        current_image = 1
+    elif current_image > total_images:
+        current_image = total_images
+
+    root.title('IQ 測驗 {}/{}'.format(current_image, total_images))
+    image_path = '{}.png'.format(current_image)
+    img = Image.open(image_path)  # 取得 image 圖像物件
+    img = img.resize((950, 400))
+    photo = ImageTk.PhotoImage(img)  # 轉成可以放在 tkinter 的資料類型
+    image_label.config(image=photo)
+    image_label.image = photo
+
+
+
+if __name__ == '__main__':
+    current_image = 1
+    total_images = 20
+
+    # 主視窗
+    root = tk.Tk()
+    root.title('IQ 測驗')
+
+    # 存放 IQ 圖
+    image_label = tk.Label(root)
+    image_label.pack()
+
+    # 建立按鈕
+    prev_button = tk.Button(root, text="上一題", command=lambda: update_image(-1))
+    prev_button.pack(side=tk.LEFT, padx=10, pady=10)
+
+    options_frame = tk.Frame(root)  # 用來放置多組的 radiobuttons
+    for option in ['A', 'B', 'C', 'D', 'E', 'F']:
+        rb = tk.Radiobutton(options_frame, text=option)
+        rb.pack(side=tk.LEFT, padx=10)
+    options_frame.pack(pady=10)
+
+    next_button = tk.Button(root, text="下一題", command=lambda: update_image(1))
+    next_button.pack(side=tk.RIGHT, padx=10, pady=10)
+
+    # 顯示第一題的圖
+    update_image(0)
+
+    # 啟動視窗
+    root.mainloop()
+
+
+
