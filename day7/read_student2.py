f5da607aaf843ece61d95ab02a2d26c460d8596d
vincentjava <vincentjava@yahoo.com.tw>
2024-02-06 09:52:08 +0800

    python

--- day7/read_student2.py
+++ day7/read_student2.py
@@ -2,8 +2,17 @@
 # 目的: 可以印出學生年齡與平均
 file = open('student.txt', 'r', encoding='UTF-8')
 rows = file.readlines()
+
+total_age = 0  # 存放年齡總數
+count = 0      # 存放筆數(學生人數)
 for row in rows[1:]:
     # '劉一 18' 變成 ['劉一', '18']
     #                  0      1
     data = row.strip().split(" ")
-    print(data[1])
+    age = int(data[1])
+    print(age)
+    total_age += age  # 累計年齡
+    count += 1        # 累計人次
+print('--------------------')
+avg = total_age / count
+print('平均年齡:', avg)
