7865dee46e8c52c4037a76427d9381aafd987b37
vincentjava <vincentjava@yahoo.com.tw>
2024-02-06 12:25:45 +0800

    python

--- day7/read_stock2.py
+++ day7/read_stock2.py
@@ -6,21 +6,25 @@
 import requests
 import datetime
 
+# 組合路徑
 date = datetime.datetime(2024, 2, 5)
 url = 'https://www.twse.com.tw/exchangeReport/BWIBBU_d?response=csv&date={}&selectType=ALL'
 url = url.format(date.strftime('%Y%m%d'))
 print(url)
 
-
-file = open('stock.txt', 'r', encoding='UTF-8')
-rows = file.readlines()
+# 取得資料
+data = requests.get(url).text
+rows = data.split("\r\n")
 
 for row in rows[2:]:
     # 資料整理: 將 " 去除, 將 - 改成 -1
     row = row.strip().replace('"', '').replace('-', '-1')
-    # 證券代號,證券名稱,殖利率(%),股利年度,本益比,股價淨值比,財報年/季
-    #    0      1       2         3     4       5        6
+    # 證券代號,證券名稱,殖利率(%),股利年度,本益比,股價淨值比,財報年/季, 空白
+    #    0      1       2         3     4       5        6         7
     data = row.split(",")
+    if len(data) < 8:
+        continue
+
     symbol = data[0]
     name = data[1]
     yield_r = float(data[2])
