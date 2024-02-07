10b3c2564b05459056eefba38413576e0a7d2055
vincentjava <vincentjava@yahoo.com.tw>
2024-02-07 10:12:20 +0800

    python

--- day8/weather.py
+++ day8/weather.py
@@ -3,7 +3,9 @@ import json
 # 天氣查詢
 # 網路位置: https://openweathermap.org/api
 # 資料位置: https://api.openweathermap.org/data/2.5/weather?q={}&appid={}
-city_name = 'taipei,TW'
+#city_name = 'taipei,TW'
+#city_name = 'kaohsiung,TW'
+city_name = 'yunlin,TW'
 key = 'a60f29f9b7623bf199279d78a529a2c0'  # 請用自己的 key
 url = 'https://api.openweathermap.org/data/2.5/weather?q={}&appid={}'
 url = url.format(city_name, key)
