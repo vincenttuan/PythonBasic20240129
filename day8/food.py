22d92a51af2f67304e375fd726685f1d28ddba05
vincentjava <vincentjava@yahoo.com.tw>
2024-02-07 12:23:03 +0800

    python

--- day8/food.py
+++ day8/food.py
@@ -6,7 +6,7 @@ import json
 url = 'https://data.moa.gov.tw/Service/OpenData/FromM/AgricultureiRiceFailure.aspx'
 json_list = json.loads(requests.get(url).text)
 
-keyword = '池上'
+keyword = '日本'
 for item in json_list:
     if keyword in item['品名']:
         print(item['品名'], item['不合格原因'])
