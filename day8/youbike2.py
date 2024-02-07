e95fcda63eff59dea2e268a39d104c9939dc5bed
vincentjava <vincentjava@yahoo.com.tw>
2024-02-07 11:23:46 +0800

    python

--- day8/youbike2.py
+++ day8/youbike2.py
@@ -1,4 +1,13 @@
+import requests
+import json
 # 計算 youbike 的使用率
 # 使用率 usage_rate = bemp/tot
 # 請印出每一個站點的使用率
+url = 'https://tcgbusfs.blob.core.windows.net/dotapp/youbike/v2/youbike_immediate.json'
+json_list = json.loads(requests.get(url).text)
+
+for youbike in json_list:
+    sna = youbike['sna']
+    usage_rate = youbike['bemp']/youbike['tot']
+    print('%s 使用率 %.1f %%' % (sna, usage_rate*100))
 
