4800c3882fd2a53bfaa7603ec76cd4a06a694750
vincentjava <vincentjava@yahoo.com.tw>
2024-02-07 09:48:21 +0800

    python

--- day8/weather.py
+++ day8/weather.py
@@ -1,3 +1,5 @@
+import requests
+import json
 # 天氣查詢
 # 網路位置: https://openweathermap.org/api
 # 資料位置: https://api.openweathermap.org/data/2.5/weather?q={}&appid={}
@@ -7,4 +9,13 @@ url = 'https://api.openweathermap.org/data/2.5/weather?q={}&appid={}'
 url = url.format(city_name, key)
 print(url)
 
+json_dict = json.loads(requests.get(url).text)
+print(json_dict)
 
+print('天氣狀況')
+print('天氣說明')
+print('現在氣溫')
+print('體感溫度')
+print('現在濕度')
+print('現在風速')
+print('雲層覆蓋')
