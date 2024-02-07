6a37a395b4b9096e9391bfad778e92910c0dfba5
vincentjava <vincentjava@yahoo.com.tw>
2024-02-07 11:29:29 +0800

    python

--- day8/youbike3.py
+++ day8/youbike3.py
@@ -6,3 +6,8 @@ import json
 url = 'https://tcgbusfs.blob.core.windows.net/dotapp/youbike/v2/youbike_immediate.json'
 json_list = json.loads(requests.get(url).text)
 
+# 為每一個站計算使用率之後並新增一個使用率屬性來存放
+for youbike in json_list:
+    youbike['usage_rate'] = float("%.2f" % (youbike['bemp']/youbike['tot'] * 100))
+
+print(json_list)
