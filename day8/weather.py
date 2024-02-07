1f6e8dd06e565d42ad9f2838138da8f290986679
vincentjava <vincentjava@yahoo.com.tw>
2024-02-07 09:28:44 +0800

    python

--- day8/weather.py
+++ day8/weather.py
@@ -1,4 +1,10 @@
 # 天氣查詢
 # 網路位置: https://openweathermap.org/api
 # 資料位置: https://api.openweathermap.org/data/2.5/weather?q={},tw&appid={}
+city_name = 'taipei,TW'
+key = 'a60f29f9b7623bf199279d78a529a2c0'  # 請用自己的 key
+url = 'https://api.openweathermap.org/data/2.5/weather?q={},tw&appid={}'
+url = url.format(city_name, key)
+print(url)
+
 
