2a102d4139eab99518fc19f7248f7b35afa04949
vincentjava <vincentjava@yahoo.com.tw>
2024-02-07 12:21:40 +0800

    python

--- day8/food.py
+++ day8/food.py
@@ -1,1 +1,13 @@
+import requests
+import json
 # 資料來源: https://data.moa.gov.tw/Service/OpenData/FromM/AgricultureiRiceFailure.aspx
+# 請找出 "品名" 中含有 "池上" 字樣的不合格米
+
+url = 'https://data.moa.gov.tw/Service/OpenData/FromM/AgricultureiRiceFailure.aspx'
+json_list = json.loads(requests.get(url).text)
+
+keyword = '池上'
+for item in json_list:
+    if keyword in item['品名']:
+        print(item['品名'], item['不合格原因'])
+
