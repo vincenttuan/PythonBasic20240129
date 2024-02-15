663841ec4a4f962364fc954f150714b546ac799f
vincentjava <vincentjava@yahoo.com.tw>
2024-02-15 10:09:16 +0800

    python

--- day9/偵測照片上的人臉.py
+++ day9/偵測照片上的人臉.py
@@ -5,4 +5,16 @@ face_cascade = cv2.CascadeClassifier('haarcascade/haarcascade_frontalface_defaul
 
 # 讀取影像檔
 frame = cv2.imread('sample_image/test.jpg')
+# print(frame)
+# 將彩色圖片轉灰階, 目的:增加處理的效率
+gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+print(gray)
+
+# 顯示圖片
+cv2.imshow('My Image', gray)
+
+# 在圖片上按下任意鍵離開
+c = cv2.waitKey(0)
+
+
 
